889e0adc150a250f3aff61286bc061c5d7799227
terrysfrancis@hotmail.com <terrysfrancis@hotmail.com>
2019-05-03 21:23:37 -0400

    Remove package-lock.json

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,12 +2,3 @@ module.exports = {
   dbName: 'movieInfoDB',
   dbUrl: 'mongodb://localhost:27017/movieInfoDB'
 };
-
-// mLab
-// module.exports = {
-//   dbName: 'movies-info',
-//   dbUrl:
-//     'mongodb://moviesInfoAdmin:M6o6v8i4e3s7@ds123929.mlab.com:23929/movies-info',
-//   dbUser: 'moviesInfoAdmin',
-//   dbPassword: 'M6o6v8i4e3s7'
-// };
